0b9aee21a40c7f85be0cf4d9d967d1ef88bbbb60
Narendra52977 <68866931+Narendra52977@users.noreply.github.com>
2020-10-21 12:19:12 +0530

    edit

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,17 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
-
-        return(
-            <>
-               
-            </>
-        )
-    }
+  render() {
+    return (
+      <>
+        <h1 data-ns-test="project-name">Online exams conducting Application</h1>
+        <h2 data-ns-test="project-description">
+          One can give all the tests which he has applied
+        </h2>
+      </>
+    );
+  }
 }
-
 
 export default App;
